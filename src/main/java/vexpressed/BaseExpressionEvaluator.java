279adfb1a977d9f0d3bf3ea019f32d37013c561f
Richard "Virgo" Richter <virgo47@gmail.com>
2016-05-12 14:01:34 +0200

    BaseExpressionEvaluator cache size can be controlled or disabled, LRU implemented with LinkedHashMap

--- src/main/java/vexpressed/BaseExpressionEvaluator.java
+++ src/main/java/vexpressed/BaseExpressionEvaluator.java
@@ -1,7 +1,7 @@
 package vexpressed;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
@@ -19,10 +19,26 @@ import vexpressed.vars.ExpressionVariableTypeResolver;
  */
 public class BaseExpressionEvaluator {
 
-	private Map<String, ParseTree> expressionCache = new ConcurrentHashMap<>();
+	public static final int DEFAULT_CACHE_CAPACITY = 50;
+
+	private final ParseCache expressionCache;
 
 	private DelegateFunctionExecutor functionExecutor = new DelegateFunctionExecutor()
 		.scanForFunctions(BasicFunctions.class);
+
+	/** Creates evaluator with specified cache size - size of 0 disables caching. */
+	public BaseExpressionEvaluator(int cacheCapacity) {
+		if (cacheCapacity > 0) {
+			expressionCache = new LruParseCache(cacheCapacity);
+		} else {
+			expressionCache = new DisabledParseCache();
+		}
+	}
+
+	/** Creates evaluator with default-sized cache. */
+	public BaseExpressionEvaluator() {
+		this(DEFAULT_CACHE_CAPACITY);
+	}
 
 	protected BaseExpressionEvaluator addFunctionsFrom(Object functionsDelegate) {
 		functionExecutor.scanForFunctions(functionsDelegate);
@@ -37,17 +53,23 @@ public class BaseExpressionEvaluator {
 		return this;
 	}
 
-	/** Evaluates expression using provided variableResolver. */
+	/**
+	 * Evaluates expression using provided variableResolver. This does not cache the result
+	 * of expression parsing.
+	 */
 	public ExpressionType check(
 		String expression, ExpressionVariableTypeResolver variableResolver)
 	{
-		ParseTree parseTree = expressionParseTree(expression);
+		ParseTree parseTree = ExpressionUtils.createParseTree(expression);
 		ExpressionValidatorVisitor visitor = new ExpressionValidatorVisitor(variableResolver)
 			.withFunctionTypeResolver(functionExecutor);
 		return visitor.visit(parseTree);
 	}
 
-	/** Evaluates expression using provided variableResolver. */
+	/**
+	 * Evaluates expression using provided variableResolver. Caches the result
+	 * of expression parsing for better performance of repeated executions.
+	 */
 	public Object eval(String expression, ExpressionVariableResolver variableResolver) {
 		ParseTree parseTree = expressionParseTree(expression);
 		ParseTreeVisitor visitor = new ExpressionCalculatorVisitor(variableResolver)
@@ -68,5 +90,53 @@ public class BaseExpressionEvaluator {
 			expressionCache.put(expression, parseTree);
 		}
 		return parseTree;
+	}
+
+	private interface ParseCache {
+
+		ParseTree get(String expression);
+
+		ParseTree put(String expression, ParseTree parseTree);
+	}
+
+	private static class LruParseCache extends LinkedHashMap<String, ParseTree>
+		implements ParseCache
+	{
+
+		private final int maxSize;
+
+		private LruParseCache(int maxSize) {
+			// max size should be like 75% loadFactor, +1 is for small caches to avoid resize
+			// number 1f used for loadFactor will never be reached
+			// true for access order => LRU
+			super((maxSize + 1) * 4 / 3, 1f, true);
+			this.maxSize = maxSize;
+		}
+
+		@Override
+		protected boolean removeEldestEntry(Map.Entry<String, ParseTree> eldest) {
+			return size() > maxSize;
+		}
+
+		@Override
+		public synchronized ParseTree get(String expression) {
+			return super.get(expression);
+		}
+
+		@Override
+		public synchronized ParseTree put(String key, ParseTree value) {
+			return super.put(key, value);
+		}
+	}
+
+	private static class DisabledParseCache implements ParseCache {
+
+		@Override public ParseTree get(String expression) {
+			return null;
+		}
+
+		@Override public ParseTree put(String expression, ParseTree parseTree) {
+			return null;
+		}
 	}
 }
