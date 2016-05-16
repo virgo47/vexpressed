084ffb65402987e84768a348ed6d1f08098c6003
Richard "Virgo" Richter <virgo47@gmail.com>
2016-05-16 16:18:00 +0200

    fixed ExpressionParam annotation before we start using it, of course it applies to PARAMETER

--- src/main/java/vexpressed/support/ExpressionParam.java
+++ src/main/java/vexpressed/support/ExpressionParam.java
@@ -6,15 +6,24 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Parameter;
 
-/** Annotates the method parameter. */
-@Target({ElementType.METHOD})
+import vexpressed.meta.ExpressionType;
+
+/**
+ * Annotates the method parameter - without parameters it is not necessary, as any method
+ * parameter that is not VariableResolver is considered as possible function parameter.
+ */
+@Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExpressionParam {
 
 	/** Name of the parameter - if empty, {@link Parameter#getName()} is used. */
 	String name() default "";
 
-	// TODO not used yet
-	/** Default value (as string) if the argument is not provided. */
-	String defaultValue();
+	/**
+	 * Default value (as string) if the argument is not provided. Default value is
+	 * supported for all recognized types of {@link ExpressionType} values except for
+	 * {@link ExpressionType#OBJECT}.
+	 */
+	// TODO what should "" mean for strings? Is it null? Can't we specify "" as default value?
+	String defaultValue() default "";
 }
