3171e44abe28b2e17b2595885a627a5f5484f505
Richard "Virgo" Richter <virgo47@gmail.com>
2016-06-14 17:34:49 +0200

    better BaseExpressionEvaluatorTest (also BasicFunction test), asInteger also tested

--- src/test/java/vexprtest/BaseExpressionEvaluatorTest.java
+++ src/test/java/vexprtest/BaseExpressionEvaluatorTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.testng.Assert.assertEquals;
 
+import vexpressed.BaseExpressionEvaluator;
+import vexpressed.core.FunctionExecutionFailed;
+import vexpressed.core.VariableResolver;
+
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,9 +15,8 @@ import java.util.Arrays;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import vexpressed.BaseExpressionEvaluator;
-import vexpressed.core.FunctionExecutionFailed;
 
+/** This tests also */
 public class BaseExpressionEvaluatorTest {
 
 	@DataProvider(name = "test-contains-data")
@@ -26,9 +29,7 @@ public class BaseExpressionEvaluatorTest {
 	}
 
 	@Test(dataProvider = "test-contains-data")
-	public void testContainsFunction(
-		String needle, boolean expectedResult)
-	{
+	public void testContainsFunction(String needle, boolean expectedResult) {
 		boolean result = new BaseExpressionEvaluator()
 			.evalBoolean("contains(haystack, needle)", var ->
 				var.equals("haystack") ? Arrays.asList("one", "two")
@@ -39,32 +40,48 @@ public class BaseExpressionEvaluatorTest {
 	}
 
 	@Test
-	public void instantVariableIsConvertedToIsoString() {
+	public void asIntegerTest() {
+		assertEquals(eval("asInteger(5)"), 5);
+		assertEquals(eval("asInteger(5.5)"), 5);
+		assertEquals(eval("asInteger(4_000_000_000/4_000_000_001)"), 0);
+		assertThatThrownBy(() -> eval("asInteger(4_000_000_000)"))
+			.isInstanceOf(FunctionExecutionFailed.class)
+			.hasRootCauseInstanceOf(ArithmeticException.class);
+	}
+
+	@Test
+	public void instantAsStringReturnsIsoString() {
 		Instant now = Instant.now();
-		Object result = new BaseExpressionEvaluator().eval("asString(var)", var -> now);
+		Object result = eval("asString(var)", var -> now);
 		assertEquals(result, now.toString());
 	}
 
 	@Test
-	public void localDateIsConvertedToIsoString() {
+	public void localDateAsStringReturnsIsoString() {
 		assertEquals(
-			new BaseExpressionEvaluator()
-				.eval("asString(var)", var -> LocalDate.of(2015, 8, 26)),
+			eval("asString(var)", var -> LocalDate.of(2015, 8, 26)),
 			"2015-08-26");
 	}
 
 	@Test
-	public void localDateTimeIsConvertedToIsoString() {
+	public void localDateTimeAsStringReturnsIsoString() {
 		assertEquals(
-			new BaseExpressionEvaluator()
-				.eval("asString(var)", var -> LocalDateTime.of(2015, 8, 26, 22, 37)),
+			eval("asString(var)", var -> LocalDateTime.of(2015, 8, 26, 22, 37)),
 			"2015-08-26T22:37:00");
 	}
 
 	@Test
 	public void nonexistentFunctionThrowsException() {
-		assertThatThrownBy(() -> new BaseExpressionEvaluator().eval("fun()", var -> null))
+		assertThatThrownBy(() -> eval("fun()", var -> null))
 			.isInstanceOf(FunctionExecutionFailed.class)
 			.hasMessageMatching("Function 'fun' was not registered");
+	}
+
+	private Object eval(String expression) {
+		return eval(expression, VariableResolver.NULL_VARIABLE_RESOLVER);
+	}
+
+	private Object eval(String expression, VariableResolver variableResolver) {
+		return new BaseExpressionEvaluator().eval(expression, variableResolver);
 	}
 }
